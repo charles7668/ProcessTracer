11617b302eea68d47162810fffc0ab4dce5297dc
charles <charles76680119@gmail.com>
2024-08-16 09:17:49 +0800

    加入專案檔案。

---
+++ ProcessTracer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessTracer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
