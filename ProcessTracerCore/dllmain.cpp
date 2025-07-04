5169b9aeaed6d761ab4963d14b08383308c118a9
charles7668 <charles76680119@gmail.com>
2025-07-04 11:24:57 +0800

    feat: include detours lib

--- ProcessTracerCore/dllmain.cpp
+++ ProcessTracerCore/dllmain.cpp
@@ -1,11 +1,17 @@
 ﻿// dllmain.cpp : 定義 DLL 應用程式的進入點。
 #include "pch.h"
+#include "detours.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
 {
+	if (DetourIsHelperProcess())
+	{
+		return TRUE;
+	}
+
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
